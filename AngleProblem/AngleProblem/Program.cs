50247c82c6ae7c6cce0936483291b1ecf90efe18
Denis Staritin(From VS) <dstaritin@gmail.com>
2016-06-29 11:36:40 +0300

    Initial Commit From VS

---
+++ AngleProblem/AngleProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleProblem
+{
+    public struct Angle
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
