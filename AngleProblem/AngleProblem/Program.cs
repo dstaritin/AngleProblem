8ba5dbc127f61843e6d8765d2870d0cd6d06bf51
Denis Staritin(From VS) <dstaritin@gmail.com>
2016-06-29 13:41:32 +0300

    Adding operator '+'

--- AngleProblem/AngleProblem/Program.cs
+++ AngleProblem/AngleProblem/Program.cs
@@ -9,42 +9,72 @@ namespace AngleProblem
     public struct Angle
     {
         private bool _pozitive;
-        private int _degrees, _minutes, _seconds;        
+        private int _degrees, _minutes, _seconds;
 
-        public Angle(bool _poz, int _deg, int _min, int _sec)
+        public bool Pozitive => _pozitive;
+
+        public int Degrees => _degrees;
+
+        public int Minutes => _minutes;
+
+        public int Seconds => _seconds;
+
+
+        public Angle(bool poz, int deg, int min, int sec)
         {
-            _pozitive = _poz;
-            _degrees = _deg;
-            _minutes = _min;
-            _seconds = _sec;
+            _pozitive = poz;
+            _degrees = deg;
+            _minutes = min;
+            _seconds = sec;
             Adduction();
         }
+
+        public Angle(Angle an)
+        {
+            _pozitive = an._pozitive;
+            _degrees = an._degrees;
+            _minutes = an._minutes;
+            _seconds = an._seconds;
+            Adduction();
+        }
+
         public override string ToString()
         {
-            char ch;
-            if (_pozitive)
-                ch = '+';
-            else
-                ch = '-';
-            return $"{ch} {_degrees}° {_minutes}\' {_seconds}\"";
+            var ch = Pozitive ? '+' : '-';
+            return $"{ch} {Degrees}° {Minutes}\' {Seconds}\"";
         }
 
         public void Adduction()
         {
-            while (_seconds > 60)
+            while (_seconds >= 60)
             {
                 _seconds -= 60;
                 _minutes++;
             }
-            while ( _minutes > 60 )
+            while (_minutes >= 60)
             {
                 _minutes -= 60;
                 _degrees++;
             }
-            while ( _degrees > 360 )
-            {
+            while (_degrees >= 360)
                 _degrees -= 360;
-            }
+        }
+        //public void Inverse()
+        //{
+        //    _pozitive = !_pozitive;
+        //    _
+        //}
+        public static Angle operator +(Angle a1, Angle a2)
+        {
+            bool poz = a1.Pozitive;
+            int deg, min, sec;
+
+            if (a1.Pozitive != a2.Pozitive)
+                /*a2.Inverse()*/;
+            deg = a1.Degrees + a2.Degrees;
+            min = a1.Minutes + a2.Minutes;
+            sec = a1.Seconds + a2.Seconds;
+            return new Angle(poz, deg, min, sec);
         }
     }
 
@@ -52,8 +82,12 @@ namespace AngleProblem
     {
         static void Main(string[] args)
         {
-            var a = new Angle(true, 369, 45, 74);
-            Console.WriteLine(a);
+            var a = new Angle(true, 59, 48, 2);
+            var b = new Angle(true, 95, 57, 1);
+            Console.WriteLine("{0}\n{1}\n", a, b);
+
+            var c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
